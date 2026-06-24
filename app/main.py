89815d8489bd4432be8fa7553202d2d723adc40c
D8S <ddipto396@gmail.com>
2026-06-24 23:58:08 +0600

    Faiss Vectore Store Integration on Implementation phase

--- app/main.py
+++ app/main.py
@@ -18,4 +18,4 @@ async def root():
 
 app.include_router(document_router, prefix="/api/v1")
 
-// FAISS Vectore Store Integration on planning phase
+// FAISS Vectore Store Integration on implenmentation phase
